f72f3df4a62bf09098a61efb4c3887d4fb6cc4ff
Stéphane Caron <stephane.caron@normalesup.org>
2023-07-25 10:22:55 +0200

    Update the cart-pole example with separate live plot

--- examples/cart_pole.py
+++ examples/cart_pole.py
@@ -27,12 +27,11 @@ import qpsolvers
 from loop_rate_limiters import RateLimiter
 
 from ltv_mpc import solve_mpc
+from ltv_mpc.live_plots import CartPolePlot
 from ltv_mpc.systems import CartPole
 
 EXAMPLE_DURATION: float = 10.0  # seconds
 NB_SUBSTEPS: int = 15  # number of integration substeps
-
-params = CartPole.Parameters()  # default cart-pole model
 
 
 def parse_command_line_arguments() -> argparse.Namespace:
@@ -66,7 +65,9 @@ def parse_command_line_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def get_target_states(state: np.ndarray, target_vel: float):
+def get_target_states(
+    cart_pole: CartPole, state: np.ndarray, target_vel: float
+):
     """Define the reference state trajectory over the receding horizon.
 
     Args:
@@ -76,10 +77,10 @@ def get_target_states(state: np.ndarray, target_vel: float):
     Returns:
         Goal state at the end of the horizon.
     """
-    nx = CartPole.STATE_DIM
-    T = params.sampling_period
-    target_states = np.zeros((params.nb_timesteps + 1) * nx)
-    for k in range(params.nb_timesteps + 1):
+    nx = cart_pole.STATE_DIM
+    T = cart_pole.sampling_period
+    target_states = np.zeros((cart_pole.nb_timesteps + 1) * nx)
+    for k in range(cart_pole.nb_timesteps + 1):
         target_states[k * nx] = state[0] + (k * T) * target_vel
         target_states[k * nx + 2] = target_vel
     return target_states
@@ -87,33 +88,31 @@ def get_target_states(state: np.ndarray, target_vel: float):
 
 if __name__ == "__main__":
     args = parse_command_line_arguments()
+    cart_pole = CartPole()
+    live_plot = CartPolePlot(cart_pole, order=args.plot)
     mpc_problem = CartPole.build_mpc_problem(
-        params,
+        cart_pole,
         terminal_cost_weight=10.0,
         stage_state_cost_weight=1.0,
         stage_input_cost_weight=1e-3,
     )
 
-    cart_pole = CartPole(
-        params,
-        initial_state=np.zeros(CartPole.STATE_DIM),
-    )
-    cart_pole.init_live_plot(order=args.plot)
-
-    dt = params.sampling_period / NB_SUBSTEPS
+    dt = cart_pole.sampling_period / NB_SUBSTEPS
     rate = RateLimiter(frequency=1.0 / (args.slowdown * dt), warn=False)
-    for t in np.arange(0.0, EXAMPLE_DURATION, params.sampling_period):
+    state = np.zeros(cart_pole.STATE_DIM)
+    for t in np.arange(0.0, EXAMPLE_DURATION, cart_pole.sampling_period):
         target_vel = 0.5 + (np.cos(t / 2.0) if args.tv_vel else 0.0)
-        target_states = get_target_states(cart_pole.state, target_vel)
-        mpc_problem.update_initial_state(cart_pole.state)
+        target_states = get_target_states(cart_pole, state, target_vel)
+        mpc_problem.update_initial_state(state)
         mpc_problem.update_goal_state(target_states[-CartPole.STATE_DIM :])
         mpc_problem.update_target_states(target_states[: -CartPole.STATE_DIM])
         plan = solve_mpc(mpc_problem, solver=args.solver)
         for step in range(NB_SUBSTEPS):
-            cart_pole.step(plan.first_input, dt)
-            cart_pole.update_live_plot(
-                plan,
+            state = cart_pole.integrate(state, plan.first_input, dt)
+            live_plot.update(
+                plan=plan,
                 plan_time=t,
+                state=state,
                 state_time=t + step * dt,
             )
             rate.sleep()
