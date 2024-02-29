5492037b02e58e300b355d4787a6cdc7a1117908
Stéphane Caron <stephane.caron@inria.fr>
2024-02-29 16:03:35 +0100

    [minor] Move SPDX identifiers up in source headers

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2022 Stéphane Caron
 # SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
 #
 # This file makes sure Python treats the test directory as a package.
